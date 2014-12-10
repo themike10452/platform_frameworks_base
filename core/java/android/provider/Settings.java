1789a81655f9a8a77d7f63161fbdd0717285f861
maxwen <max.weninger@gmail.com>
2014-12-10 01:48:47 +0100

    [1/2] base: allow disabling volume adjust sound
    Change-Id: I95893180865209d2aeaf4b3fa129a74df7523bbe

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -2843,6 +2843,13 @@ public final class Settings {
         public static final String VOLUME_KEYS_CONTROL_MEDIA_STREAM = "volume_keys_control_media_stream";
 
         /**
+         * Volume Adjust Sounds Enable, This is the noise made when using volume hard buttons
+         * Defaults to 1 - sounds enabled
+         * @hide
+         */
+        public static final String VOLUME_ADJUST_SOUND = "volume_adjust_sound";
+
+        /**
          * Settings to backup. This is here so that it's in the same place as the settings
          * keys and easy to update.
          *
@@ -6793,6 +6800,7 @@ public final class Settings {
          * @hide
          */
         public static final String REQUIRE_PASSWORD_TO_DECRYPT = "require_password_to_decrypt";
+
 
         /**
          * Settings to backup. This is here so that it's in the same place as the settings
