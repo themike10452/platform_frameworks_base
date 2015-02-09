CyanogenMod/android_frameworks_base

frameworks: implement system-wide Torch service
Allows Torch and any camera to coexist peacefully. When torch is active
and a camera is opened, the torch will be first shut down, and the
camera will be allowed to continue opening, instead of displaying
"Cannot connect to camera."

Also a squash of the following commits:

    add framework torch service

    Change-Id: I0c7a055b33fdebe697f23d207b20408e06eb74a8
    Signed-off-by: Roman Birg <roman@cyngn.com>

    Fully fix interaction between torch and camera usage.

    For doing this, two things are necessary:
    - The torch shutdown needs to be synchronous, so that the torch app has
      a chance to release the camera before the camera app tries to use it.
    - The torch service needs to track camera usage, so that it can notify
      the torch app if a camera is busy.

    Change-Id: I521091d255ff4c251d97a0e8d65c9d2a80b9dae4

    Fix camera usage tracking.

    When a Camera object is finalized, it notifies the torch service that
    it's no longer using the camera. This call used to not check whether the
    caller actually is still using the camera and called release() before,
    so depending on timing of the finalizer, it could happen that e.g. the
    camera app released the camera user record of the torch app, leading to
    the torch usage not being detected on next usage of the camera app.

    Fix that by checking whether the caller of onCameraClosed() actually
    owned the camera prior to releasing the record.
    Also fix synchronization between accesses.

    Change-Id: Ic01cbc3c89ab8d855e4613bfa1b2f548de919b73

    frameworks: extend TorchService to be able to handle sysfs torch config

    Some devices are still using a sysfs node to control the torch. Let's
    support those configurations and properly shut the torch down.

    Change-Id: I5fa1cccdcffad26dbfef4b2ee0eafe1218a5308b
    Signed-off-by: Roman Birg <roman@cyngn.com>

    frameworks: move TorchService to new services location

    Chan

packages/SystemUI/src/com/android/systemui/qs/tiles/FlashlightTile.java
@@ -17,33 +17,34 @@
 package com.android.systemui.qs.tiles;
 
 import android.app.ActivityManager;
+import android.content.Context;
+import android.hardware.TorchManager;
 import android.os.SystemClock;
 
 import com.android.systemui.R;
 import com.android.systemui.qs.QSTile;
-import com.android.systemui.statusbar.policy.FlashlightController;
 
 /** Quick settings tile: Control flashlight **/
 public class FlashlightTile extends QSTile<QSTile.BooleanState> implements
-        FlashlightController.FlashlightListener {
+        TorchManager.TorchCallback {
 
     /** Grace period for which we consider the flashlight
      * still available because it was recently on. */
     private static final long RECENTLY_ON_DURATION_MILLIS = 500;
 
-    private final FlashlightController mFlashlightController;
+    private final TorchManager mTorchManager;
     private long mWasLastOn;
 
     public FlashlightTile(Host host) {
         super(host);
-        mFlashlightController = host.getFlashlightController();
-        mFlashlightController.addListener(this);
+        mTorchManager = (TorchManager) mContext.getSystemService(Context.TORCH_SERVICE);
+        mTorchManager.addListener(this);
     }
 
     @Override
     protected void handleDestroy() {
         super.handleDestroy();
-        mFlashlightController.removeListener(this);
+        mTorchManager.removeListener(this);
     }
 
     @Override
@@ -53,6 +54,7 @@ public class FlashlightTile extends QSTile<QSTile.BooleanState> implements
 
     @Override
     public void setListening(boolean listening) {
+
     }
 
     @Override
@@ -65,7 +67,7 @@ public class FlashlightTile extends QSTile<QSTile.BooleanState> implements
             return;
         }
         boolean newState = !mState.value;
-        mFlashlightController.setFlashlight(newState);
+        mTorchManager.setTorch(newState);
         refreshState(newState);
     }
 
@@ -90,7 +92,7 @@ public class FlashlightTile extends QSTile<QSTile.BooleanState> implements
 
         // Always show the tile when the flashlight is or was recently on. This is needed because
         // the camera is not available while it is being used for the flashlight.
-        state.visible = mWasLastOn != 0 || mFlashlightController.isAvailable();
+        state.visible = mWasLastOn != 0 || mTorchManager.isAvailable();
         state.label = mHost.getContext().getString(R.string.quick_settings_flashlight_label);
         state.iconId = state.value
                 ? R.drawable.ic_qs_flashlight_on : R.drawable.ic_qs_flashlight_off;
@@ -110,17 +112,17 @@ public class FlashlightTile extends QSTile<QSTile.BooleanState> implements
     }
 
     @Override
-    public void onFlashlightOff() {
+    public void onTorchOff() {
         refreshState(false);
     }
 
     @Override
-    public void onFlashlightError() {
+    public void onTorchError() {
         refreshState(false);
     }
 
     @Override
-    public void onFlashlightAvailabilityChanged(boolean available) {
+    public void onTorchAvailabilityChanged(boolean available) {
         refreshState();
     }
 

themike10452/platform_frameworks_base

packages/SystemUI/src/com/android/systemui/qs/tiles/FlashlightTile.java
@@ -17,33 +17,34 @@
 package com.android.systemui.qs.tiles;
 
 import android.app.ActivityManager;
+import android.content.Context;
+import android.hardware.TorchManager;
 import android.os.SystemClock;
 
 import com.android.systemui.R;
 import com.android.systemui.qs.QSTile;
-import com.android.systemui.statusbar.policy.FlashlightController;
 
 /** Quick settings tile: Control flashlight **/
 public class FlashlightTile extends QSTile<QSTile.BooleanState> implements
-        FlashlightController.FlashlightListener {
+        TorchManager.TorchCallback {
 
     /** Grace period for which we consider the flashlight
      * still available because it was recently on. */
     private static final long RECENTLY_ON_DURATION_MILLIS = 500;
 
-    private final FlashlightController mFlashlightController;
+    private final TorchManager mTorchManager;
     private long mWasLastOn;
 
     public FlashlightTile(Host host) {
         super(host);
-        mFlashlightController = host.getFlashlightController();
-        mFlashlightController.addListener(this);
+        mTorchManager = (TorchManager) mContext.getSystemService(Context.TORCH_SERVICE);
+        mTorchManager.addListener(this);
     }
 
     @Override
     protected void handleDestroy() {
         super.handleDestroy();
-        mFlashlightController.removeListener(this);
+        mTorchManager.removeListener(this);
     }
 
     @Override
@@ -53,6 +54,7 @@ public class FlashlightTile extends QSTile<QSTile.BooleanState> implements
 
     @Override
     public void setListening(boolean listening) {
+
     }
 
     @Override
@@ -65,7 +67,7 @@ public class FlashlightTile extends QSTile<QSTile.BooleanState> implements
             return;
         }
         boolean newState = !mState.value;
-        mFlashlightController.setFlashlight(newState);
+        mTorchManager.setTorchEnabled(newState);
         refreshState(newState);
     }
 
@@ -90,7 +92,7 @@ public class FlashlightTile extends QSTile<QSTile.BooleanState> implements
 
         // Always show the tile when the flashlight is or was recently on. This is needed because
         // the camera is not available while it is being used for the flashlight.
-        state.visible = mWasLastOn != 0 || mFlashlightController.isAvailable();
+        state.visible = mWasLastOn != 0 || mTorchManager.isAvailable();
         state.label = mHost.getContext().getString(R.string.quick_settings_flashlight_label);
         state.iconId = state.value
                 ? R.drawable.ic_qs_flashlight_on : R.drawable.ic_qs_flashlight_off;
@@ -110,17 +112,17 @@ public class FlashlightTile extends QSTile<QSTile.BooleanState> implements
     }
 
     @Override
-    public void onFlashlightOff() {
+    public void onTorchOff() {
         refreshState(false);
     }
 
     @Override
-    public void onFlashlightError() {
+    public void onTorchError() {
         refreshState(false);
     }
 
     @Override
-    public void onFlashlightAvailabilityChanged(boolean available) {
+    public void onTorchAvailabilityChanged(boolean available) {
         refreshState();
     }
 