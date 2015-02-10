e9221f3f4a795d980758155c16d6c9607a930496
dankoman <dankoman30@gmail.com>
2015-02-10 20:37:15 +0100

    Layers : fix sensitive lockscreen notification title and time
    fix this crap when no overlay is being utilized:
    http://i.imgur.com/XjW5igU.png
    
    Change-Id: I5e10e5d4bb4249278649fa9c58120932c2dd7195

--- packages/SystemUI/src/com/android/systemui/statusbar/BaseStatusBar.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/BaseStatusBar.java
@@ -1375,6 +1375,7 @@ public abstract class BaseStatusBar extends SystemUI implements
             } catch (NameNotFoundException e) {
                 title.setText(entry.notification.getPackageName());
             }
+            title.setTextColor(R.color.exposed_primary_text_light);
 
             final ImageView icon = (ImageView) publicViewLocal.findViewById(R.id.icon);
             final ImageView profileBadge = (ImageView) publicViewLocal.findViewById(
@@ -1419,6 +1420,7 @@ public abstract class BaseStatusBar extends SystemUI implements
                 time.setVisibility(View.VISIBLE);
                 time.setTime(entry.notification.getNotification().when);
             }
+            time.setTextColor(R.color.exposed_primary_text_light);
 
             final TextView text = (TextView) publicViewLocal.findViewById(R.id.text);
             if (text != null) {
